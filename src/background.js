71bda02d82d123afae77213eaf798109a43ebe6b
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-26 12:38:30 +0300

    Update;- extension loads successfully

---
+++ src/background.js
@@ -0,0 +1,54 @@
+// Copyright 2019 LarCity LLC
+//
+// (From The Chromium Authors circa 2018:)
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Guide for background pages: https://developer.chrome.com/extensions/background_pages
+
+declare const chrome: any;
+
+chrome.runtime.onInstalled.addListener(function() {
+    // // @TODO Arbitrary task on installed - try opening a new tab here (for signup)
+    // chrome.storage.sync.set({
+    //   color: '#3aa757'
+    // }, function() {
+    //   console.log('The color is green.');
+    // });
+  
+    // Set rules for when popup should show
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            // hostEquals: 'developer.chrome.com',
+            schemes: ['http', 'https']
+          },
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+  
+    // // // Open a new tab
+    // // chrome.tabs.create({
+    // //   url: 'popup.html'
+    // // }, function() { /** Callback action here */});
+  
+  
+    // // Docs: https://developer.chrome.com/extensions/webNavigation
+    // chrome.webNavigation.onCompleted.addListener(function() {
+    //   console.log('User is running an app in developer mode');
+    // }, {
+    //   url: [
+    //     // Filter by URL
+    //     {
+    //       urlMatches: 'http://localhost:4200'
+    //     },
+    //     // Attempting to filter by the chrome plugin (dev) ID
+    //     {
+    //       hostSuffix: 'akkcpblckifneccnbpjnbompdenfnlji'
+    //     }
+    //   ]
+    // });
+  
+  });
