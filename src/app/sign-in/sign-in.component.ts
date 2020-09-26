4e161764341c9b003e69ed76f08d183fa98a98a4
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-26 19:37:08 +0300

    Update;- set up minimal scaffold for signIn feature

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+signInGroup: FormGroup;
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signInGroup = this._fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
 }
