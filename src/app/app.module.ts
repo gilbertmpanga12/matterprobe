36e8bb3b0101105d8778d3ac0b97eeb4dcb76887
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-26 16:21:58 +0300

    Update;- generated required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
