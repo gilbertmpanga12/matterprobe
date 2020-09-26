121f432bf71f8394156b189f8795da205241e117
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-26 19:59:02 +0300

    Update;- minor changes to commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     SignInComponent,
-    DashboardComponent
+    DashboardComponent,
+    ForgotpasswordComponent
   ],
   imports: [
     BrowserModule, ReactiveFormsModule
