6520764438fc4f316fb9be472ea2b9c210e903f0
gilbertmpanga12 <gilbertmpanga.gm@gmail.com>
2020-09-26 19:51:39 +0300

    Update;- created new account

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signUpGroup: FormGroup;
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    
+    this.signUpGroup = this._fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
   }
 
 }
